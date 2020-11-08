1ddfdabd384836bf5f06cda71005a51a2fc4972d
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2020-11-08 11:20:43 +0700

    update notify dan upupjs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// $("#prodi").select2();
+$.notify("Cobain deh kl ga percaya.", "info");
+$.notify("Biar yang koneksinya susah bisa akses.", "info");
+$.notify("Tool bisa diakses secara offline!", "info");
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
